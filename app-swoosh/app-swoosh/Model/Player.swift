c7d3523a2b26b2cb9b0c285d23be401a7be888d0
Todd Leach <todd.leach.r@gmail.com>
2017-10-17 16:12:03 -0400

    created IBActions for the league buttons

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Nexion MacBook on 10/17/17.
+//  Copyright © 2017 Nexion. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkill: String?
+}
